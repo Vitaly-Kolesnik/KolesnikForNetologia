24b0ca150f5c81248debc5c5c029eeab2f8f5183
Vitaly_Kolesnik <81166419+Vitaly-Kolesnik@users.noreply.github.com>
2025-12-05 05:49:30 +0300

    Add files via upload

---
+++ Lesson3/Lesson3Task4 fix.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <windows.h>
+
+int main() {
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    std::setlocale(LC_ALL, "Russian");
+
+    int a, b;
+    std::cout << "Введите целое число: ";
+    std::cin >> a;
+    std::cout << "Введите целое число: ";
+    std::cin >> b;
+
+    if (a <= -100 || a >= 100 || b <= -100 || b >= 100) {
+        std::cout << "Ошибка! Одно из чисел вне диапазона!" << std::endl;
+        return EXIT_SUCCESS;
+    }
+
+    int temp = a;
+    if (temp < 0) {
+        std::cout << "минус ";
+        temp = -temp;
+    }
+
+    int tens = temp / 10;
+    int ones = temp % 10;
+
+    switch (tens) {
+    case 9: std::cout << "девяносто"; break;
+    case 8: std::cout << "восемьдесят"; break;
+    case 7: std::cout << "семьдесят"; break;
+    case 6: std::cout << "шестьдесят"; break;
+    case 5: std::cout << "пятьдесят"; break;
+    case 4: std::cout << "сорок"; break;
+    case 3: std::cout << "тридцать"; break;
+    case 2: std::cout << "двадцать"; break;
+    case 1:
+        switch (ones) {
+        case 0: std::cout << "десять"; return 0;
+        case 1: std::cout << "одиннадцать"; return 0;
+        case 2: std::cout << "двенадцать"; return 0;
+        case 3: std::cout << "тринадцать"; return 0;
+        case 4: std::cout << "четырнадцать"; return 0;
+        case 5: std::cout << "пятнадцать"; return 0;
+        case 6: std::cout << "шестнадцать"; return 0;
+        case 7: std::cout << "семнадцать"; return 0;
+        case 8: std::cout << "восемнадцать"; return 0;
+        case 9: std::cout << "девятнадцать"; return 0;
+        }
+    case 0: break;
+    }
+
+    if (tens > 1) std::cout << " ";
+
+    switch (ones) {
+    case 1: std::cout << "один"; break;
+    case 2: std::cout << "два"; break;
+    case 3: std::cout << "три"; break;
+    case 4: std::cout << "четыре"; break;
+    case 5: std::cout << "пять"; break;
+    case 6: std::cout << "шесть"; break;
+    case 7: std::cout << "семь"; break;
+    case 8: std::cout << "восемь"; break;
+    case 9: std::cout << "девять"; break;
+    case 0: if (tens == 0) std::cout << "ноль"; break;
+    }
+
+    if (a < b) std::cout << " меньше чем ";
+    else if (a > b) std::cout << " больше чем ";
+    else std::cout << " равно ";
+
+    temp = b;
+    if (temp < 0) {
+        std::cout << "минус ";
+        temp = -temp;
+    }
+
+    tens = temp / 10;
+    ones = temp % 10;
+
+    switch (tens) {
+    case 9: std::cout << "девяносто"; break;
+    case 8: std::cout << "восемьдесят"; break;
+    case 7: std::cout << "семьдесят"; break;
+    case 6: std::cout << "шестьдесят"; break;
+    case 5: std::cout << "пятьдесят"; break;
+    case 4: std::cout << "сорок"; break;
+    case 3: std::cout << "тридцать"; break;
+    case 2: std::cout << "двадцать"; break;
+    case 1:
+        switch (ones) {
+        case 0: std::cout << "десять"; return 0;
+        case 1: std::cout << "одиннадцать"; return 0;
+        case 2: std::cout << "двенадцать"; return 0;
+        case 3: std::cout << "тринадцать"; return 0;
+        case 4: std::cout << "четырнадцать"; return 0;
+        case 5: std::cout << "пятнадцать"; return 0;
+        case 6: std::cout << "шестнадцать"; return 0;
+        case 7: std::cout << "семнадцать"; return 0;
+        case 8: std::cout << "восемнадцать"; return 0;
+        case 9: std::cout << "девятнадцать"; return 0;
+        }
+    case 0: break;
+    }
+
+    if (tens > 1) std::cout << " ";
+
+    switch (ones) {
+    case 1: std::cout << "один"; break;
+    case 2: std::cout << "два"; break;
+    case 3: std::cout << "три"; break;
+    case 4: std::cout << "четыре"; break;
+    case 5: std::cout << "пять"; break;
+    case 6: std::cout << "шесть"; break;
+    case 7: std::cout << "семь"; break;
+    case 8: std::cout << "восемь"; break;
+    case 9: std::cout << "девять"; break;
+    case 0: if (tens == 0) std::cout << "ноль"; break;
+    }
+
+    std::cout << std::endl;
+    return EXIT_SUCCESS;
+}
+
