a16c41ba27adbd33f1c47dd6abb2f747b55dbadd
Vitaly_Kolesnik <81166419+Vitaly-Kolesnik@users.noreply.github.com>
2026-08-17 19:52:55 +0300

    Add files via upload

---
+++ Block6/Lesson10/Task3/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+template<typename T>
+class UniquePtr {
+public:
+    explicit UniquePtr(T* ptr = nullptr) : ptr_(ptr) {}
+
+    ~UniquePtr() {
+        delete ptr_;
+        std::cout << "UniquePtr destructor called (deleted managed object)\n";
+    }
+
+    UniquePtr(const UniquePtr&) = delete;
+    UniquePtr& operator=(const UniquePtr&) = delete;
+    T& operator*() const {
+        return *ptr_;
+    }
+
+    T* operator->() const {
+        return ptr_;
+    }
+
+    T* release() {
+        T* tmp = ptr_;
+        ptr_ = nullptr;
+        return tmp;
+    }
+
+    T* get() const {
+        return ptr_;
+    }
+
+private:
+    T* ptr_;
+};
+
+
+int main() {
+    std::cout << "=== Create UniquePtr<int> with value 42 ===\n";
+    UniquePtr<int> up(new int(42));
+
+    std::cout << "*up = " << *up << std::endl;
+    std::cout << "up-> (нет члена, но оператор работает) -> " << up.get() << std::endl;
+
+    std::cout << "\n=== Release ownership ===\n";
+    int* raw = up.release();
+    std::cout << "Raw pointer value: " << *raw << std::endl;
+    std::cout << "After release, up.get() = " << up.get() << " (nullptr)\n";
+
+    delete raw;
+
+    std::cout << "\n=== Going out of scope (up will be destroyed) ===\n";
+    return 0;
+}
